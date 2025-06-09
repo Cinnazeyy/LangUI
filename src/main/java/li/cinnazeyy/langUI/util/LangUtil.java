82c2352b8a5b7d2f3763c11650f71f6622578723
Zoriot <zoriot@proton.me>
2025-06-09 13:56:47 +0200

    🔧 Add missing translations (ko_KR, pt_PT, ru_RU, zh_CN, zh_TW)
    To align with LL & Plot-System.

--- src/main/java/li/cinnazeyy/langUI/util/LangUtil.java
+++ src/main/java/li/cinnazeyy/langUI/util/LangUtil.java
@@ -6,7 +6,6 @@ import li.cinnazeyy.langlibs.core.LangLibAPI;
 import li.cinnazeyy.langlibs.core.language.Language;
 import li.cinnazeyy.langlibs.core.file.LanguageFile;
 import li.cinnazeyy.langlibs.core.language.LanguageUtil;
-import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.Plugin;
 
 public class LangUtil extends LanguageUtil {
@@ -18,8 +17,13 @@ public class LangUtil extends LanguageUtil {
         LangLibAPI.register(plugin, new LanguageFile[]{
                 new LanguageFile(plugin, 1.0, Language.en_GB),
                 new LanguageFile(plugin, 1.0, Language.de_DE, "de_AT", "de_CH"),
-                new LanguageFile(plugin, 1.0, Language.fr_FR),
-                new LanguageFile(plugin, 1.0, Language.he_IL)
+                new LanguageFile(plugin, 1.0, Language.fr_FR, "fr_CA"),
+                new LanguageFile(plugin, 1.0, Language.pt_PT, "pt_BR"),
+                new LanguageFile(plugin, 1.0, Language.ko_KR),
+                new LanguageFile(plugin, 1.0, Language.ru_RU, "ba_RU", "tt_RU"),
+                new LanguageFile(plugin, 1.0, Language.zh_CN),
+                new LanguageFile(plugin, 1.0, Language.zh_TW, "zh_HK"),
+                new LanguageFile(plugin, 1.0, Language.he_IL),
         });
         langUtilInstance = new LangUtil();
     }
@@ -30,10 +34,5 @@ public class LangUtil extends LanguageUtil {
 
     public static LangUtil getInstance() {
         return langUtilInstance;
-    }
-
-    @Override
-    public String get(CommandSender sender, String key) {
-        return super.get(sender, key);
     }
 }
