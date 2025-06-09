55adf31c848208e0ee56883f717fee3be2285904
Cinnazeyy <imcinnazeyy@gmail.com>
2025-06-09 13:28:05 +0200

    add hebrew translation

--- src/main/java/li/cinnazeyy/langUI/util/LangUtil.java
+++ src/main/java/li/cinnazeyy/langUI/util/LangUtil.java
@@ -7,16 +7,19 @@ import li.cinnazeyy.langlibs.core.language.Language;
 import li.cinnazeyy.langlibs.core.file.LanguageFile;
 import li.cinnazeyy.langlibs.core.language.LanguageUtil;
 import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.Plugin;
 
 public class LangUtil extends LanguageUtil {
     private static LangUtil langUtilInstance;
 
     public static void init() {
         if (langUtilInstance != null) return;
-        LangLibAPI.register(LangUI.getPlugin(), new LanguageFile[]{
-                new LanguageFile(LangUI.getPlugin(), 1.0, Language.en_GB),
-                new LanguageFile(LangUI.getPlugin(), 1.0, Language.de_DE, "de_AT", "de_CH"),
-                new LanguageFile(LangUI.getPlugin(), 1.0, Language.fr_FR)
+        Plugin plugin = LangUI.getPlugin();
+        LangLibAPI.register(plugin, new LanguageFile[]{
+                new LanguageFile(plugin, 1.0, Language.en_GB),
+                new LanguageFile(plugin, 1.0, Language.de_DE, "de_AT", "de_CH"),
+                new LanguageFile(plugin, 1.0, Language.fr_FR),
+                new LanguageFile(plugin, 1.0, Language.he_IL)
         });
         langUtilInstance = new LangUtil();
     }
